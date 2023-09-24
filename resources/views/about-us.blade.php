c08621f0ed8a7b4743f099a410f039298652c55a
srxo <salmaramdani01@gmail.com>
2023-09-24 14:52:48 +0200

    Initial commit

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<title>Porschefy</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1>welcome</h1>
+    <p>
+        lorem ipsum
+    </p>
+</article>
+</body>
